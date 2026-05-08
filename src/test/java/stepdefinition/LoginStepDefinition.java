2913f7d4c77b4163f32821997ea37edae81f4118
RajvinderKaur <rajvindergahlla91@gmail.com>
2026-05-08 00:39:33 -0400

    step definition file

---
+++ src/test/java/stepdefinition/LoginStepDefinition.java
@@ -0,0 +1,49 @@
+package stepdefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefinition {
+	@Given("Chrome browser is opened")
+	public void chrome_browser_is_opened() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Given("user is on url {string}")
+	public void user_is_on_url(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("user clicks on Form Authentication link")
+	public void user_clicks_on_form_authentication_link() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("user should land to Login page")
+	public void user_should_land_to_login_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("user enters correct username {string}")
+	public void user_enters_correct_username(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("correct password {string}")
+	public void correct_password(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("enter Login button")
+	public void enter_login_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("user should be successfully logged in")
+	public void user_should_be_successfully_logged_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
